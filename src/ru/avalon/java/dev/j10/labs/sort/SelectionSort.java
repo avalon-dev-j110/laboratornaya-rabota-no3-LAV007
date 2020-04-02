665b80bcb66d2214fdda73551526b0ecf812bda7
Alex <61654925+LAV007@users.noreply.github.com>
2020-04-02 12:45:26 +0300

    Update SelectionSort.java

--- src/ru/avalon/java/dev/j10/labs/sort/SelectionSort.java
+++ src/ru/avalon/java/dev/j10/labs/sort/SelectionSort.java
@@ -18,6 +18,22 @@ public class SelectionSort implements Sort {
      * {@inheritDoc}
      */
     public void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+        int min = array[i];
+        int minId = i;
+        for (int j = i+1; j < array.length; j++) {
+            if (array[j] < min) {
+                min = array[j];
+                minId = j;
+            }
+        }
+        // замена
+        if (i != min) {
+        int temp = array[i];
+        array[i] = min;
+        array[minId] = temp;
+        }
+    }
         /*
          * TODO(Студент): Реализовать метод sort класса SelectionSort
          */
